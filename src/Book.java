222fb920130cdc478be52460282b1cfe1e65672a
origin <viktoryaa2000@mail.ru>
2024-11-29 16:25:43 +0300

    commit2

--- src/Book.java
+++ src/Book.java
@@ -3,4 +3,11 @@ public class Book {
     int year;
     String author;
 
+    public Book(String title, int year, String author) {
+        this.title = title;
+        this.year = year;
+        this.author = author;
+
+    }
+
 }
